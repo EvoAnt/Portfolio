9463de0a32e14d9d9d0d3ce6dc0abdd4ab7611d4
Anthony Lara <Capone0824@gmail.com>
2023-10-11 18:42:27 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>My Portfolio</h1>
+      <h2>Under Construction</h2>
+    </div>
+  );
+}
+
+export default App;
